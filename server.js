84e296525480c33a594c5ca3e7d978297ec038c8
drthisguy <page.c.tyler@gmail.com>
2020-02-09 18:34:19 -0500

    server.js file added and setup to listen on port and load index.html

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    path = require('path'),
+    journal = require('./journal.json');
+
+const app = express(),
+    PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+  });
+  
+
+
+  app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+  });
