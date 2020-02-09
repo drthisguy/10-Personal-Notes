07a87c8f32534616431f89c6650ee188e8d89262
drthisguy <page.c.tyler@gmail.com>
2020-02-09 18:44:23 -0500

    response added to /notes to load the note taking page.

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.get("/", (req, res) => {
   });
   
 
-
-  app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
   });
+  
+
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
